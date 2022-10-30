c77d84fcaa583e16c890c2733538dfbe46c66e0f
Михаил Сидоренко <smikhai1@mbp-mihail.local>
2022-10-30 19:30:08 +0300

    make data generation script efficient

--- generate.py
+++ generate.py
@@ -8,16 +8,20 @@
 
 """Generate images using pretrained network pickle."""
 
+from functools import partial
+import multiprocessing as mp
 import os
+import os.path as osp
 import re
 from typing import List, Optional
 
+import cv2
 import click
 import dnnlib
 import numpy as np
 import PIL.Image
 import torch
-
+from tqdm import tqdm
 import legacy
 
 #----------------------------------------------------------------------------
@@ -32,6 +36,16 @@ def num_range(s: str) -> List[int]:
     vals = s.split(',')
     return [int(x) for x in vals]
 
+
+def convert_img_to_bytes(img, format='.png'):
+    img_bgr = cv2.cvtColor(img[0], cv2.COLOR_RGB2BGR)
+    success, encoded_image = cv2.imencode(format, img_bgr)
+    if not success:
+        raise RuntimeError('Image wasn\'t encoded!')
+    bytes = encoded_image.tobytes()
+    return bytes
+
+
 #----------------------------------------------------------------------------
 
 @click.command()
@@ -43,6 +57,8 @@ def num_range(s: str) -> List[int]:
 @click.option('--noise-mode', help='Noise mode', type=click.Choice(['const', 'random', 'none']), default='const', show_default=True)
 @click.option('--projected-w', help='Projection result file', type=str, metavar='FILE')
 @click.option('--outdir', help='Where to save the output images', type=str, required=True, metavar='DIR')
+@click.option('--bs', type=int, default=16, help='Size of the batch for generation in batch-mode')
+@click.option('--num_images', type=int, help='Number of images to generate in batch-mode')
 def generate_images(
     ctx: click.Context,
     network_pkl: str,
@@ -51,7 +67,9 @@ def generate_images(
     noise_mode: str,
     outdir: str,
     class_idx: Optional[int],
-    projected_w: Optional[str]
+    projected_w: Optional[str],
+    bs: Optional[int],
+    num_images: Optional[int]
 ):
     """Generate images using pretrained network pickle.
 
@@ -76,7 +94,16 @@ def generate_images(
     # Render an image from projected W
     python generate.py --outdir=out --projected_w=projected_w.npz \\
         --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
+
+    \b
+    # Generate 10000 images in batch mode with batch size 16
+    python generate.py --outdir=out --trunc=0.85 --network=path_to_network_pkl --num_images=10000 --bs=16
+
     """
+    if projected_w is None and seeds is None and num_images is None:
+        raise ValueError('One of \'projected_w\', \'seeds\', and \'num_images\' arguments must be specified!')
+    if projected_w is None and seeds is not None and num_images is not None:
+        raise ValueError('Only one of \'seeds\' and \'num_images\' arguments must be specified!')
 
     print('Loading networks from "%s"...' % network_pkl)
     device = torch.device('cuda')
@@ -99,9 +126,6 @@ def generate_images(
             img = PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/proj{idx:02d}.png')
         return
 
-    if seeds is None:
-        ctx.fail('--seeds option is required when not using --projected-w')
-
     # Labels.
     label = torch.zeros([1, G.c_dim], device=device)
     if G.c_dim != 0:
@@ -113,12 +137,30 @@ def generate_images(
             print ('warn: --class=lbl ignored when running on an unconditional network')
 
     # Generate images.
-    for seed_idx, seed in enumerate(seeds):
-        print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
-        z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
-        img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
-        img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-        PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
+    if num_images is not None:
+        # batch mode
+        for seed in tqdm(range(int(np.ceil(num_images / bs)))):
+            z = torch.from_numpy(np.random.RandomState(seed).randn(min(bs, num_images), G.z_dim)).to(device)
+            imgs = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
+            imgs = (imgs.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+            imgs = imgs.cpu().numpy()
+            imgs = np.split(imgs, np.arange(1, len(imgs)), axis=0)
+
+            with mp.Pool(bs) as pool:
+                imgs_bytes = pool.map(convert_img_to_bytes, imgs)
+
+            for idx in range(len(imgs_bytes)):
+                path = osp.join(outdir, f'seed{seed:06d}_idx{idx:02d}.png')
+                with open(path, 'wb') as f:
+                    f.write(imgs_bytes[idx])
+    else:
+        # default non-batch mode
+        for seed_idx, seed in tqdm(enumerate(seeds), total=len(seeds)):
+            print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
+            z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
+            img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
+            img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+            PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
 
 
 #----------------------------------------------------------------------------
